ee4512e428c2fab59d6f3a8e5e7fb14cc3a02c43
tukkedd <jeamki2501@gmail.com>
2024-09-29 23:58:40 -0400

    installing dependence, node server config and html/css

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import logger from 'morgan'
+
+
+const port = process.env.PORT ?? 3000
+
+const app = express()
+app.use(logger('dev'))
+
+app.get('/', (req, res) => {
+    res.send('<h1>this is the chat</h1>')
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port https://localhost:${port}`);
+})
