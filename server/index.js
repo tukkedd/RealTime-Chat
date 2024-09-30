21ad8e41b9f913ff3db215c354cd728cfae4aeb4
tukkedd <jeamki2501@gmail.com>
2024-09-30 17:38:13 -0400

    installling socket.io

--- server/index.js
+++ server/index.js
@@ -1,16 +1,31 @@
 import express from 'express'
 import logger from 'morgan'
 
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
 
 const port = process.env.PORT ?? 3000
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+    console.log('a user has connected');
+
+    socket.on('disconnect', () => {
+        console.log('an user has been disconect');
+        
+    })
+
+})
+
 app.use(logger('dev'))
 
 app.get('/', (req, res) => {
-    res.send('<h1>this is the chat</h1>')
+    res.sendFile(process.cwd() + '/client/index.html')
 })
 
-app.listen(port, () => {
-    console.log(`Server running on port https://localhost:${port}`);
+server.listen(port, () => {
+    console.log(`Server running on port http://localhost:${port}`);
 })
